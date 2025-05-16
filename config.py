99ff385e10fa7c5e99bbf5187d4fa819ea60c125
Akito <elbandito237@gmail.com>
2025-05-16 10:12:43 +0500

    config.py - delete comment

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = "dfgdfgdfggse4325345ergsertg34t"  # ← поставь надёжный ключ сюда
+    SECRET_KEY = "dfgdfgdfggse4325345ergsertg34t"
     UPLOAD_FOLDER = os.path.join("static", "uploads")
     ALLOWED_EXTENSIONS = {'jpg', 'csv'}
